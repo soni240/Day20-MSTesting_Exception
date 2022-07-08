f36c04288e4a11de77849e680e3adea95b9a97d2
soni240 <s.fegade21@gmail.com>
2022-07-08 18:37:54 +0530

    [Gunavanti]Added:UC3_AnalyzeMood_throw_CustomeException

--- MoodAnalyze_Testing/UnitTest1.cs
+++ MoodAnalyze_Testing/UnitTest1.cs
@@ -8,21 +8,46 @@ namespace MoodAnalyze_Testing
     {
         private MoodAnalyzer moodAnalyzer;
 
+
         [TestMethod]
-        [DataRow(null)]
-        public void Given_NULLMood_shouldReturn_HAPPY(string message) //Test method
+        public void Given_NULLMood_shouldReturn_MoodAnalyzeCustomeException_NUllMessage() //Test method
         {
-            // >> Arrange
-            string expected = "HAPPY";  //Expexted Output stored in string varaible
-            moodAnalyzer = new MoodAnalyzer(message); //creating object of MOodAnalyzer class with passing param message
+            try
+            {
+                // >> Arrange
+                string message = null;  //Expexted Output stored in string varaible
+                moodAnalyzer = new MoodAnalyzer(message); //creating object of MOodAnalyzer class with passing param message
 
-            // >> Act
-            string Result = moodAnalyzer.AnalyzeMood(); //calling Analyzemood methd and storing returned string in result
+                // >> Act
+                string Result = moodAnalyzer.AnalyzeMood(); //calling Analyzemood methd and storing returned string in result
 
-            // >> Assert
-            Assert.AreEqual(expected, Result); //Comparing our expected output with result returned from analyzeMood methd
-
+            }
+            catch (MoodAnalyzer_CustomException e)//cathing the exception thrown by analyzeMood methd
+            {
+                // >> Assert
+                Assert.AreEqual("Message can not be NULL!!", e.Message);
+            }
         }
 
+        [TestMethod]
+        public void Given_EMptyMood_shouldReturn_MoodAnalyzerCustomeException_EMptyMessage() //Test method
+        {
+            try
+            {
+                // >> Arrange
+                string message = "";  //Expexted Output stored in string varaible
+                moodAnalyzer = new MoodAnalyzer(message); //creating object of MOodAnalyzer class with passing param message
+
+                // >> Act
+                string Result = moodAnalyzer.AnalyzeMood(); //calling Analyzemood methd and storing returned string in result
+
+            }
+            catch (MoodAnalyzer_CustomException e) //cathing the exception thrown by analyzeMood methd
+            {
+                // >> Assert
+                Assert.AreEqual("Message Should Not Be EMPTY!!", e.Message);
+            }
+
+        }
     }
 }
